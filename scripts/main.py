167cb4d3fd6c62223af623d94057aea295b8c1b4
liaoyd16 <liaoyd16@mails.tsinghua.edu.cn>
2020-12-29 21:11:02 +0800

    merge @20201229

--- scripts/main.py
+++ scripts/main.py
@@ -11,15 +11,15 @@ import do_phone_gender_vals
 
 def main():
     # TIMIT_proc
-    """ doc jsons """
-    prepare_doc_jsons.do_speaker_info()
-    prepare_doc_jsons.input_phones()
-    """ table jsons """
-    do_gender_sp.do_gender_sp()
-    do_sp_sent.do_sp_sent()
-    do_phone_sentdur.do_phone_sentdur()
-    do_phone_gender_sentdur.do_phone_gender_sentdur()
-    do_phone_gender_pair.do_phone_gender_pair()
+    # """ doc jsons """
+    # prepare_doc_jsons.do_speaker_info()
+    # prepare_doc_jsons.input_phones()
+    # """ table jsons """
+    # do_gender_sp.do_gender_sp()
+    # do_sp_sent.do_sp_sent()
+    # do_phone_sentdur.do_phone_sentdur()
+    # do_phone_gender_sentdur.do_phone_gender_sentdur()
+    # do_phone_gender_pair.do_phone_gender_pair()
     """ scripts """
     do_phone_gender_vals.do_phone_gender_vals()
     # display.display()
